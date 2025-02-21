9df399560a148ff67f08e5173c1b2c12d3308a8a
Andrew Nesbitt <andrewnez@gmail.com>
2025-02-21 17:27:26 +0000

    Add bootstrap-icons-helper gem and update application styles and meta information

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def meta_title
+    [@meta_title, 'Ecosyste.ms: Licenses'].compact.join(' | ')
+  end
+
+  def meta_description
+    @meta_description || app_description
+  end
+
+  def app_name
+    "Licenses"
+  end
+
+  def app_description
+    'An open API service to parse license metadata from many open source software ecosystems.'
+  end
 end
